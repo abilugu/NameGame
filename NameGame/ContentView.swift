ddb62bcba7dd6d710c96544734abec16462e2afe
Aravind Bilugu <abilugu@gmail.com>
2025-08-24 15:16:04 -0700

    Initial commit: NameGame iOS project

---
+++ NameGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NameGame
+//
+//  Created by Aravind Bilugu on 8/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NameGame/NameGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  NameGameApp.swift
+//  NameGame
+//
+//  Created by Aravind Bilugu on 8/24/25.
+//
+
+import SwiftUI
+
+@main
+struct NameGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
